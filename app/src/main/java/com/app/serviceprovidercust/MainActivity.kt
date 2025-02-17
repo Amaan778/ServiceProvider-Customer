637cb4f4eb787aba4d1a9782a90a4b74ccf81785
Amaan Ansari <amaanansari778@gmail.com>
2025-02-17 15:43:23 +0530

    Service Provider Customer

--- app/src/main/java/com/app/serviceprovidercust/MainActivity.kt
+++ app/src/main/java/com/app/serviceprovidercust/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.app.serviceprovidercust
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.app.serviceprovidercust.boarding.OnBoardingScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,OnBoardingScreen::class.java))
+            finish()
+        },3000)
+
     }
 }
