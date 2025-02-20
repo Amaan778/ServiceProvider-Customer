5bc0c35b50f52742870df14b068d408901b0f64e
Amaan Ansari <amaanansari778@gmail.com>
2025-02-20 05:43:26 +0530

    Service Provider Customer

---
+++ app/src/main/java/com/app/serviceprovidercust/boarding/OnBoardingPageAdapter.kt
@@ -0,0 +1,4 @@
+package com.app.serviceprovidercust.boarding
+
+class OnBoardingPageAdapter {
+}
--- app/src/main/java/com/app/serviceprovidercust/boarding/OnBoardingScreen.kt
+++ app/src/main/java/com/app/serviceprovidercust/boarding/OnBoardingScreen.kt
@@ -11,5 +11,8 @@ class OnBoardingScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding_screen)
+
+
+
     }
 }
