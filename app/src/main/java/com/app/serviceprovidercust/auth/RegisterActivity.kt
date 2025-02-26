937e3569149c6a14928064a93db6754256cd972e
Amaan Ansari <amaanansari778@gmail.com>
2025-02-27 05:24:10 +0530

    Service Provider Customer

--- app/src/main/java/com/app/serviceprovidercust/auth/RegisterActivity.kt
+++ app/src/main/java/com/app/serviceprovidercust/auth/RegisterActivity.kt
@@ -1,16 +1,24 @@
 package com.app.serviceprovidercust.auth
 
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.app.serviceprovidercust.R
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var firstname: EditText
@@ -26,5 +34,94 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        firstname=findViewById(R.id.firstname)
+        lastname=findViewById(R.id.lastname)
+        emil=findViewById(R.id.email)
+        number=findViewById(R.id.phonenumber)
+        password=findViewById(R.id.passwordEditText)
+        btn=findViewById(R.id.create)
+
+        auth = Firebase.auth
+        database = Firebase.database.reference
+
+        //        For password visibility
+        val passwordInputLayout = findViewById<TextInputLayout>(R.id.passwordInputLayout)
+
+        // Set the initial icon to the "eye-off" drawable (password hidden)
+//        passwordInputLayout.endIconDrawable = ContextCompat.getDrawable(this, R.drawable.eyeclose)
+
+        // Variable to keep track of password visibility state
+        var isPasswordVisible = false
+
+        // Toggle password visibility when the end icon is clicked
+        passwordInputLayout.setEndIconOnClickListener {
+            isPasswordVisible = !isPasswordVisible
+
+            if (isPasswordVisible) {
+                // Show password
+                password.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+//                passwordInputLayout.endIconDrawable = ContextCompat.getDrawable(this, R.drawable.eyeopen)
+            } else {
+                // Hide password
+                password.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+//                passwordInputLayout.endIconDrawable = ContextCompat.getDrawable(this, R.drawable.eyeclose)
+            }
+
+            // Move the cursor to the end after toggling
+            password.setSelection(password.text.length)
+        }
+
+        btn.setOnClickListener {
+
+            val first=firstname.text.toString().trim()
+            val last=lastname.text.toString().trim()
+            val mail=emil.text.toString().trim()
+            val mobile=number.text.toString().trim()
+            val pass=password.text.toString().trim()
+
+            if (first.isEmpty()){
+                firstname.error="Required"
+            }
+            else if (last.isEmpty()){
+                lastname.error="Required"
+            }
+            else if (mail.isEmpty()){
+                emil.error="Required"
+            }
+            else if (mobile.isEmpty()){
+                number.error="Required"
+            }
+            else if (pass.isEmpty()){
+                password.error="Required"
+            }
+            else{
+
+
+                auth.createUserWithEmailAndPassword(mail,pass)
+                    .addOnCompleteListener(this){task->
+                        if (task.isSuccessful){
+
+                            val uid=auth.currentUser?.uid
+                            val userd=AuthDataClass(first,last,mail,mobile,pass)
+
+                            database.child("users").child(uid!!).setValue(userd)
+                            startActivity(Intent(this,LoginActivity::class.java))
+                            finish()
+                            Toast.makeText(this,"Account created sucessfully", Toast.LENGTH_LONG).show()
+                            diaog?.dismiss()
+
+                        }else{
+                            Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT,).show()
+                            diaog?.dismiss()
+                        }
+
+                    }
+
+            }
+
+
+        }
+
     }
 }
