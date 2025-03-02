6543fecf94212f48c328a16cdf89627166bd4d47
Amaan Ansari <amaanansari778@gmail.com>
2025-03-02 16:45:49 +0530

    Service Provider Customer

--- app/src/main/java/com/app/serviceprovidercust/auth/LoginActivity.kt
+++ app/src/main/java/com/app/serviceprovidercust/auth/LoginActivity.kt
@@ -1,13 +1,27 @@
 package com.app.serviceprovidercust.auth
 
+import android.app.Dialog
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.app.serviceprovidercust.R
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var btn: Button
+    private lateinit var reg: TextView
+    private lateinit var auth: FirebaseAuth
+    private var diaog: Dialog?=null
+    var sharedPreferences: SharedPreferences?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
